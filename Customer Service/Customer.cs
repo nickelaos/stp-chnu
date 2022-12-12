41e66c1a9db78f2e8c6e87cc346017a66bf22321
Nick Gutsuliak <nick@seprism.com>
2022-12-12 23:34:57 +0200

    Add lab8

---
+++ Customer Service/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Customer_Service
+{
+    [DataContract(Namespace = "Customer_Service")]
+    public class Customer
+    {
+        [DataMember]
+        public Int32 CustomerID { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public String Address { get; set; }
+    }
+}
---
+++ Customer Service/IPhoneService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Customer_Service
+{
+    [ServiceContract]
+    public interface IPhoneService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Wrapped,
+        UriTemplate = "GetPhones")]
+        List<Phone> GetPhoneList();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+        BodyStyle = WebMessageBodyStyle.Wrapped,
+        UriTemplate = "GetPhones/{id}")]
+        Phone GetPhoneById(string id);
+    }
+}
