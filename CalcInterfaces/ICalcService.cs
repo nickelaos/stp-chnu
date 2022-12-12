6f04a3e755e538076f2b434287ba25c4a6ec259d
Nick Gutsuliak <nick@seprism.com>
2022-12-12 23:28:29 +0200

    Add lab6

---
+++ CalcInterfaces/ICalcService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace CalcInterfaces
+{
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        float Sum(float firstNumber, float secondNumber);
+
+        [OperationContract]
+        float Substraction(float firstNumber, float secondNumber);
+
+        [OperationContract]
+        float Multiplication(float firstNumber, float secondNumber);
+
+        [OperationContract]
+        string Divizion(float firstNumber, float secondNumber);
+
+        [OperationContract]
+        float Mod(float firstNumber, float secondNumber);
+
+        [OperationContract]
+        float Power(float firstNumber, float secondNumber);
+
+
+    }
+}
