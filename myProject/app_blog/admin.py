976ecdd2c02674c56f70ed759828445b3b164d58
Nick Gutsuliak <nick@seprism.com>
2022-12-12 23:10:03 +0200

    Add lab3

--- myProject/app_blog/admin.py
+++ myProject/app_blog/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
 
-# Register your models here.
+from .forms import ArticleImageForm
+from .models import Article, ArticleImage, Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("category", )
+    prepopulated_fields = {"slug": ("category", )}
+    fieldsets = (
+        ('', {
+            "fields": ("category", "slug", ),
+        }),
+    )
+
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    form = ArticleImageForm
+    extra = 0
+
+    fieldsets = (
+        ('', {
+            "fields": ("title", "image", ),
+        }),
+    )
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "pub_date", "slug", "main_page")
+    inlines = [ArticleImageInline]
+    multiupload_form = True
+    multiupload_list = False
+    prepopulated_fields = {"slug": ("title", )}
+    raw_id_fields = ("category", )
+    fieldsets = (
+        ('', {
+            "fields": ("pub_date", "category", "title", "description", "main_page"),
+        }),
+        ("Additionally", {
+            "classes": ("grp-collapse grp-closed", ),
+            "fields": ("slug", ),
+        }),
+    )
+
+    def delete_file(self, pk, request):
+        obj = get_object_or_404(ArticleImage, pk=pk)
+        return obj.delete()
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
--- myProject/app_blog/models.py
+++ myProject/app_blog/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField("Category", max_length=250, help_text="Max length 250 symbols")
+    slug = models.SlugField("Slug")
+
+    class Meta:
+        verbose_name = "Category for publication"
+        verbose_name_plural = "Category for publications"
+
+    def __str__(self):
+        return self.category
+
+
+class Article(models.Model):
+    title = models.CharField("Caption", max_length=250, help_text="Max length 250 symbols")
+    description = models.TextField(blank=True, verbose_name="Description")
+    pub_date = models.DateTimeField("Publication`s date", default=timezone.now)
+    slug = models.SlugField("Slug", unique_for_date="pub_date")
+    main_page = models.BooleanField("Main Page", default=False, help_text="Show on main page")
+    category = models.ForeignKey(Category, related_name="articles", blank=True, null=True, verbose_name="Category",
+                                 on_delete=models.CASCADE)
+
+    objects = models.Manager()
+
+    class Meta:
+        ordering = ["-pub_date"]
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        try:
+            url = reverse(
+                'news-detail',
+                kwargs={
+                    "yaer": self.pub_date.strftime("%Y"),
+                    "month": self.pub_date.strftime("%m"),
+                    "day": self.pub_date.strftime("%d"),
+                    "slug": self.slug
+                    }
+            )
+        except:
+            url = "/"
+        return url
+
+
+class ArticleImage(models.Model):
+    article = models.ForeignKey(Article, verbose_name="Article", related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField("Photo", upload_to="photos")
+    title = models.CharField("Caption", max_length=250, help_text="Max 250 symbols", blank=True)
+
+    class Meta:
+        verbose_name = "Photo for article"
+        verbose_name_plural = "Photo for article"
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def filename(self):
+        return self.image.name.rsplit('/', 1)[-1]
